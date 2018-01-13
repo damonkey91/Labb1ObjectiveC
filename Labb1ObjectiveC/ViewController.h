ac84cd2593b63c02087262f9bc7216186e872f75
mrx <tobjo057@student.liu.se>
2018-01-13 14:38:20 +0100

    Initial Commit

---
+++ Labb1ObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Labb1ObjectiveC
+//
+//  Created by lösen är 0000 on 2018-01-13.
+//  Copyright © 2018 lösen är 0000. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Labb1ObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Labb1ObjectiveC
+//
+//  Created by lösen är 0000 on 2018-01-13.
+//  Copyright © 2018 lösen är 0000. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
